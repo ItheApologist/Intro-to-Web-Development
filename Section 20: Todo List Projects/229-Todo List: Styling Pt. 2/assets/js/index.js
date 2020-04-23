71c6f482c80718599595deb362bb67f9ee89db61
Thomas Hennessy <twh0144@gmail.com>
2020-04-23 16:52:43 -0400

    Pusing new Sections to Codebase. Starting on Backend lessions.

---
+++ Section 20: Todo List Projects/229-Todo List: Styling Pt. 2/assets/js/index.js
@@ -0,0 +1,22 @@
+// Function to strike through the list item when clicked on.
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed")
+});
+
+
+// Function to remove list items when the X is clicked on. 
+$("ul").on("click", "span", function(i) {
+    $(this).parent().fadeOut(function() {
+        $(this).remove();
+    });
+    i.stopPropagation()
+});
+
+// Adds items to the todo list from text entered in input. It also re-calls the strike and remove functions to update the list. 
+$("input").keypress(function(i) {
+    if(i.which === 13) {
+        var val = $(this).val();
+        $(this).val("")
+        $("ul").append(`<li><span><i class="fa fa-trash-alt"></i></span> ${val}</li>`);
+    }
+})
---
+++ Section 22: Backend Basics/242-Introduction to Backend/notes.md
@@ -0,0 +1,29 @@
+# Internet Basics Notes
+
+## Static vs Dynamic Web Pages
+
+Dynamic web pages are sites complied on the server side where the server is pulling together or constructing a web page before it is sent ack as a response to an http request.
+
+Static web pages service up the same content each time for each user.
+
+## Generic Stack
+
+A stack refers to the specific language and technology choices for the backend.
+
+A general overview of a stick is like below:
+
+Backend Lanuange/ App - Server - Database
+---
+HTML/CSS/JS
+
+Reddit uses the following stack:
+
+Python/Flask - nginx - postgresql
+---
+HTML/CSS/JS
+
+Our Stack
+
+Node JS - Express - MongoDB
+---
+HTML/CSS/JS
