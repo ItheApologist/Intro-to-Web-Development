9477424c728c724b857c26e356951c13137afbf5
Thomas Hennessy <twh0144@gmail.com>
2020-04-27 14:35:43 -0400

    Pusing second set of changes

---
+++ Section 22: Backend Basics/264-Faker Exercise/listProducts.js
@@ -0,0 +1,10 @@
+var faker = require("faker");
+    product = faker.commerce.productName();
+    prices = faker.commerce.price();
+    
+for (var i = 0; i < 10; i++) {
+    console.log(`${product} ${prices}`);
+    product = faker.commerce.productName();
+    prices = faker.commerce.price();
+
+}
