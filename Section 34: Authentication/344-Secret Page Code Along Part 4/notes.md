9938c01ad0660467730ab3b309476f345526c431
Thomas Hennessy <twh0144@gmail.com>
2020-05-20 12:34:21 -0400

    Pushing compelted section to repo

---
+++ Section 34: Authentication/344-Secret Page Code Along Part 4/notes.md
@@ -0,0 +1,3 @@
+# Authentication Notes
+
+Middleware is code run in the middle of a route request.  
---
+++ Section 34: Authentication/345-Secret Page Code Along Part 5/app.js
@@ -0,0 +1,84 @@
+var express                 = require("express");
+    app                     = express();
+    mongoose                = require("mongoose");
+    passport                = require("passport");
+    bodyParser              = require("body-parser");
+    LocalStrategy           = require("passport-local");
+    passportLocalMongoose   = require("passport-local-mongoose");
+    User                    = require("./models/user");
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+};
+
+mongoose.connect("mongodb://localhost:27017/auth_demo_app");
+
+app.use(require("express-session")({
+    secret: "Rusty is the best and cutest dog in the world",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// ROUTES
+
+app.get("/", (req,res) => {
+    res.render("home");
+});
+
+app.get("/secret", isLoggedIn, (req, res) => {
+    res.render("secret");
+});
+
+// AUTH ROUTES
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    req.body.username;
+    req.body.password;
+    User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/secret");
+        });
+    });
+});
+
+// LOGIN ROUTES
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "login",
+}), (req,res) => {});
+
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
+
+app.listen(3000, () => {
+    console.log("Server hosted at http://localhost:3000");
+});
